87c507694b943f9eda872b2f4324f76eac63a84d
nagornaya.a <an.n.ag.75767@gmail.com>
2022-04-28 19:44:43 +0300

    Repair

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u0000006/data/www/faq-reg.ru/project_name')
-sys.path.insert(1, '/var/www/u0000006/data/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project_name.settings'
+sys.path.insert(0, '/var/www/u1666879/data/www/obshagana100.ru/Online-school')
+sys.path.insert(1, '/var/www/u1666879/data/djangoenv/lib/python3.8/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Online-school.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
